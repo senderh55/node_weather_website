a33aadb14742cb9e2be7b2dc7470cd6bc7f82f0e
senderh55 <senderh55@gmail.com>
2022-10-27 09:51:13 +0300

    added wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,13 +8,15 @@ const forecast = (latitude, longitude, callback) => {
     longitude;
 
   request({ url, json: true }, (error, { body }) => {
+    console.log(body.current);
     if (error) {
       callback("Unable to connect the Weather Server!", undefined);
     } else if (body.error) {
       callback("Unable to find weather for this location", undefined);
     } else {
-      const { weather_descriptions, temperature, feelslike } = body.current;
-      dataDescription = `${weather_descriptions} throughout the day. It is currently ${temperature} degrees out. and feels like ${feelslike} degrees`;
+      const { weather_descriptions, temperature, feelslike, wind_speed } =
+        body.current;
+      dataDescription = `${weather_descriptions} throughout the day. It is currently ${temperature} degrees out. and feels like ${feelslike} degrees\n Wind speed is ${wind_speed} kmh`;
       callback(undefined, dataDescription);
     }
   });
